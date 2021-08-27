08d3dbf82b81feaf8db04d6c2e830ede40fa4dfb
Wanderson Bueno <wanolibueno@gmail.com>
2021-08-27 01:47:00 -0300

    painel com telas adm

--- resources/views/adm/cadastroPessoas.blade.php
+++ resources/views/adm/cadastroPessoas.blade.php
@@ -30,24 +30,25 @@
                                 </div>
                             </div>
                             <div class="row mb-4">
-                                <div class="col-md-3">
+                                <div class="col-md-4">
                                     <div class="form-floating">
                                         <input class="form-control" id="inputCidade" type="text" placeholder="Sexo" />
                                         <label for="inputCidade">Sexo</label>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-4">
                                     <div class="form-floating mb-3">
                                         <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
                                         <label for="inputEmail">E-mail</label>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-md-4">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
                                         <label for="inputPassword">Senha</label>
                                     </div>
                                 </div>
+                                <!--
                                 <div class="col-md-3">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <label for="inputSelect"></label>
@@ -57,7 +58,7 @@
                                             <option value="2">Aluno</option>
                                         </select>
                                     </div>
-                                </div>
+                                </div>-->
                             </div>
                             
                             <div class="mt-4 mb-0">
